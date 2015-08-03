2eb3fba9e6c17eec66b7f5f2e97dcec31fba8fd6
hectorfhurtado <hurtado.fernando@gmail.com>
2015-08-03 14:03:19 -0500

    Terminamos el layout para cuando no hay JavaScript y el cargador de modulos

---
+++ html/modulos/elementos/Elementos.js
@@ -0,0 +1,10 @@
+/**
+ * @author Héctor Fernando Hurtado
+ * 
+ * @description Se encarga de tener una referencia a los elementos del DOM de la app
+ */
+
+( function() {
+    
+    alert( 'hola mundo' );
+})();
