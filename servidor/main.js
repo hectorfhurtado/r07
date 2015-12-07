318944338c8b27ec8eb572070e1f1debaa8f3ac4
Hector Hurtado <hurtado.fernando@gmail.com>
2015-12-07 06:00:18 -0500

    Subimos el servidor y es necesario seguir modificando el SVG por Chrome

--- servidor/main.js
+++ servidor/main.js
@@ -9,11 +9,20 @@ const PATH_WEB = '../html';
 
 const servidor = http.createServer( function( req, res )
 {
-	if ( /\//.test( req.url ))
+	if ( /\/$|index\.html$/.test( req.url ))
 	{
 		res.writeHead( 'Content-type', 'text/html' );
 		fs.createReadStream( path.join( __dirname, PATH_WEB + '/index.html' )).pipe( res );
+		return;
 	}
+	
+	if ( /\.html$/.test( req.url ))      res.writeHead( 'Content-type', 'text/html' );
+	else if ( /\.js$/.test( req.url ))   res.writeHead( 'Content-type', 'application/javascript' );	
+	else                                 return;
+	
+	fs.createReadStream( path.join( __dirname,  PATH_WEB + req.url )).pipe( res );
 });
 
+
 servidor.listen( 7000, '127.0.0.1' );
+console.log( 'Oyendo en el puerto 7000...' );
