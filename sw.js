1071b2dfd1e6cab36e4b13299ca387cfcff64429
Hector Hurtado <hurtado.fernando@gmail.com>
2016-01-11 17:11:01 -0500

    más actualización del path

--- sw.js
+++ sw.js
@@ -34,13 +34,13 @@ self.addEventListener( 'fetch', function enFetch( e )
 	e.respondWith( caches.match( `/r07${ e.request }` ).then( function responde( respuesta )
 	{
 		// hacemos igual la petición por si hay alguna nueva versión del archivo
-		fetch( e.request.clone() ).then( function haceElFetch( respuestaFetch )
+		fetch(  `/r07${ e.request.clone() }` ).then( function haceElFetch( respuestaFetch )
 		{
 			if ( !respuestaFetch || respuestaFetch.status != 200 ) return;
 			
 			caches
 				.open( NOMBRE_CACHE )
-				.then( cache => cache.put( e.request, respuestaFetch.clone() ));
+				.then( cache => cache.put( `/r07${ e.request }`, respuestaFetch.clone() ));
 		}).catch( error => console.log( 'No alcancé al servidor' ));
 		
 		if ( respuesta ) return respuesta;
