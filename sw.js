099087a4ec3abe3573a411512bc1e19d5f49744c
Hector Hurtado <hurtado.fernando@gmail.com>
2016-01-11 17:24:21 -0500

    volvemos a dejar el sw como estaba

--- sw.js
+++ sw.js
@@ -30,19 +30,17 @@ self.addEventListener( 'install', function enInstall( evento )
 });
 
 self.addEventListener( 'fetch', function enFetch( e )
-{
-	console.log( e.request )
-	
-	e.respondWith( caches.match( `/r07${ e.request }` ).then( function responde( respuesta )
+{	
+	e.respondWith( caches.match( e.request ).then( function responde( respuesta )
 	{
 		// hacemos igual la petición por si hay alguna nueva versión del archivo
-		fetch(  `/r07${ e.request.clone() }` ).then( function haceElFetch( respuestaFetch )
+		fetch(  `${ e.request.clone() }` ).then( function haceElFetch( respuestaFetch )
 		{
 			if ( !respuestaFetch || respuestaFetch.status != 200 ) return;
 			
 			caches
 				.open( NOMBRE_CACHE )
-				.then( cache => cache.put( `/r07${ e.request }`, respuestaFetch.clone() ));
+				.then( cache => cache.put( e.request, respuestaFetch.clone() ));
 		}).catch( error => console.log( 'No alcancé al servidor' ));
 		
 		if ( respuesta ) return respuesta;
