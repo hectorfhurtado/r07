ef580a24769d3a15de13a2867e8e002ee95142da
hectorfhurtado <hurtado.fernando@gmail.com>
2016-07-19 17:05:10 -0500

    A Esdras le faltaba una S

--- modulos/editor/Editor.js
+++ modulos/editor/Editor.js
@@ -15,7 +15,7 @@
 		[
 			'Génesis',     'Éxodo',        'Levítico',         'Números',          'Deuteronomio',    'Josué',        'Jueces',
 			'Rut',         '1 Samuel',     '2 Samuel',         '1 Reyes',          '2 Reyes',         '1 Crónicas',   '2 Crónicas',
-			'Edras',       'Nehemías',     'Ester',            'Job',              'Salmos',          'Proverbios',   'Eclesiastés',
+			'Esdras',       'Nehemías',     'Ester',            'Job',              'Salmos',          'Proverbios',   'Eclesiastés',
 			'Cantares',    'Isaías',       'Jeremías',         'Lamentaciones',    'Ezequiel',        'Daniel',       'Oseas',
 			'Joel',        'Amós',         'Abdías',           'Jonás',            'Miqueas',         'Nahúm',        'Habacuc',
 			'Sofonías',    'Hageo',        'Zacarías',         'Malaquías',        'Mateo',           'Marcos',       'Lucas',
